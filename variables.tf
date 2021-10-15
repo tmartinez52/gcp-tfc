9bef56d10c4b071ed6f2b5bdc9930a68d7abdb0b
Tim Martinez <34745639+tmartinez52@users.noreply.github.com>
2021-10-14 23:16:38 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,11 @@
+variable "project" { }
+
+variable "credentials_file" {}
+
 variable "region" {
-  description = "GCP region"
-  default     = "us-central1"
+    default = "us-central1"
 }
 
-variable "instance_type" {
-  description = "Type of Compute Engine instance to provision"
-  default     = "e2-small"
-}
-
-variable "instance_name" {
-  description = "Compute instance name"
-  default     = "Provisioned by Terraform"
-}
-variable "project_id" {
-  description = "Project ID"
-  default = "Project_ID"
+variable "zone" {
+    default = "us-central1-c"
 }
