1e8dc8fe1080b5e00cb4604dfac3750626e1f3a7
Tim Martinez <34745639+tmartinez52@users.noreply.github.com>
2021-10-14 23:16:07 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,54 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
 provider "google" {
-  project     =  var.project
-  region      =  var.region
-  zone        =  var.zone
+  credentials = file(var.credentials_file)
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
 }
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-
-  network_interface {
-    # A default network is created for all GCP projects
-    network = "default"
-    access_config {
-    }
-  }
-}
 resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = "true"
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+    name = "terraform-instance"
+    machine_type = "f1-micro"
+    tags = ["web", "dev"]
+
+    boot_disk {
+      initialize_params{
+        image = "cos-cloud/cos-stable"
+      }
+    }
+
+    network_interface {
+      network = google_compute_network.vpc_network.name
+      access_config{
+      }
+    }
+}
+resource "google_compute_instance" "vm_instance_two" {
+    name = "terraform-instance-two"
+    machine_type = "f1-micro"
+
+
+    boot_disk {
+      initialize_params{
+        image = "debian-cloud/debian-9"
+      }
+    }
+
+    network_interface {
+      network = google_compute_network.vpc_network.name
+      access_config{
+      }
+    }
 }
