3a2fa25fc3b7bbdd743f6f7f42777074e849384e
Tim Martinez <34745639+tmartinez52@users.noreply.github.com>
2021-10-14 23:33:59 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "3.5.0"
-    }
-  }
-}
-
 provider "google" {
   project     = var.project
   region      = var.region
