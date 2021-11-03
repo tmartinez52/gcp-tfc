eccb5a9572fb12b26e247261767c17c7dddca9b6
Tim Martinez <34745639+tmartinez52@users.noreply.github.com>
2021-11-03 12:36:53 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ provider "google" {
 }
 
 resource "google_service_account" "github_builder" {
-  account_id = "github_builder"
-  display_name = "github_builder"
+  account_id = "github-builder"
+  display_name = "github-builder"
 }
