ae2dc9eff1447077cf8a70ad9bb6488a3d093638
Tim Martinez <34745639+tmartinez52@users.noreply.github.com>
2021-11-03 12:28:17 -0600

    creating service account

--- main.tf
+++ main.tf
@@ -4,55 +4,7 @@ provider "google" {
   zone        = var.zone
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
-}
-
-resource "google_compute_instance" "vm_instance" {
-    name = "terraform-instance"
-    machine_type = "f1-micro"
-
-    boot_disk {
-      initialize_params{
-        image = "cos-cloud/cos-stable"
-      }
-    }
-
-    network_interface {
-      network = google_compute_network.vpc_network.name
-      access_config{
-      }
-    }
-}
-resource "google_compute_instance" "vm_instance_two" {
-    name = "terraform-instance-two"
-    machine_type = "f1-micro"
-
-
-    boot_disk {
-      initialize_params{
-        image = "debian-cloud/debian-9"
-      }
-    }
-
-    network_interface {
-      network = google_compute_network.vpc_network.name
-      access_config{
-      }
-    }
-}
-
-resource "google_sql_database" "database" {
-  name     = "my-database"
-  instance = google_sql_database_instance.sql_db.name
-}
-
-resource "google_sql_database_instance" "sql_db" {
-  name   = "my-db-instance"
-  region = var.region
-  settings {
-    tier = "db-f1-micro"
-  }
-
-  deletion_protection  = "false"
+resource "google_service_account" "service_acccount" {
+  account_id = "service-account-id"
+  display_name = "Service Account"
 }
